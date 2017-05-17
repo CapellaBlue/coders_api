fe712c9ffc4407adc882b7e2f7151266c6e784e4
029281 <sheilayuan@C02T511YHF1T.home>
2017-05-17 15:42:28 -0400

    Rails setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+
+  resources :posts, only: [:create, :index, :show] do
+    resources :comments, only: [:create, :index]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
