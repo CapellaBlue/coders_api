0421499efb5929f39e6f7c769b186cf8732bad02
029281 <sheilayuan@C02T511YHF1T.home>
2017-05-22 20:25:36 -0400

    modified for admin on heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -21,7 +21,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins '*'
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :options, :head]
+      methods: [:get, :post, :put, :options, :delete, :head]
       # methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
