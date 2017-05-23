0891d4cf68dfe508586a3206cba5744d8de487ba
029281 <sheilayuan@C02T511YHF1T.home>
2017-05-22 20:15:01 -0400

    update cors.rb for admin on heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
   allow do
-    origins 'localhost:3001'
+    # origins 'localhost:3001'
+    origins 'https://dashboard.heroku.com/apps/typepolitikappadmin'
+
     resource '*',
       #headers: 'x-domain-token',
       headers: :any,
